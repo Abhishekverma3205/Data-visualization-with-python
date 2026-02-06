500b87a640ad1c9d10e2da430a6dc80c08c388c8
Abhishek Verma <Vermad3205@gmail.com>
2026-02-06 19:14:03 +0530

    Line plot graph

---
+++ Line plot .py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+tata = [5,6,7,9,8,-2,4]
+days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+plt.plot(days,tata)
+plt.title("Stoke prices as per days in week")
+plt.xlabel("Days")
+plt.ylabel("Stoke Price")
+plt.show()
